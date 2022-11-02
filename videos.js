41198523e268308f5542ee6f0b14abe56c0173ee
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-11-02 19:34:36 +0000

    Add files via upload

--- videos.js
+++ videos.js
@@ -208,9 +208,9 @@ var SAOtitles = [
 
 var show = "SAO"
 var epNum = "1"
-var showCodes = SAO
-var showNums = SAOnums
-var showTitles = SAOtitles
+var showCodes = demonSlayer
+var showNums = demonSlayerNums
+var showTitles = demonSlayerTitles
 
 // window.onload = function() {
 //     var pickedShow = localStorage.getItem("chosenShow");
@@ -241,8 +241,8 @@ function nextEp() {
         var epTitle = showTitles[epNum-1]
         console.log(epID)
         document.querySelector(".video_screen").src = "https://docs.google.com/file/d/" + epID + "/preview"
-        document.querySelector(".episode_num").textContent = localStorage.getItem("chosenShow")
-        document.querySelector(".episode_title").textContent = localStorage.getItem("chosenShow")
+        document.querySelector(".episode_num").textContent = sessionStorage.getItem("chosenShow")
+        document.querySelector(".episode_title").textContent = sessionStorage.getItem("chosenShow")
     }
     else {
         epNum = 1
@@ -251,8 +251,8 @@ function nextEp() {
         var epTitle = showTitles[epNum-1]
         console.log(epID)
         document.querySelector(".video_screen").src = "https://docs.google.com/file/d/" + epID + "/preview"
-        document.querySelector(".episode_num").textContent = localStorage.getItem("chosenShow")
-        document.querySelector(".episode_title").textContent = localStorage.getItem("chosenShow")
+        document.querySelector(".episode_num").textContent = sessionStorage.getItem("chosenShow")
+        document.querySelector(".episode_title").textContent = sessionStorage.getItem("chosenShow")
     }
 }
 function prevEp() {
@@ -264,8 +264,8 @@ function prevEp() {
         var epTitle = showTitles[epNum-1]
         console.log(epID)
         document.querySelector(".video_screen").src = "https://drive.google.com/file/d/" + epID + "/preview"
-        document.querySelector(".episode_num").textContent = localStorage.getItem("chosenShow")
-        document.querySelector(".episode_title").textContent = localStorage.getItem("chosenShow")
+        document.querySelector(".episode_num").textContent = sessionStorage.getItem("chosenShow")
+        document.querySelector(".episode_title").textContent = sessionStorage.getItem("chosenShow")
     }
     else {
         epNum = showCodes.length
@@ -274,7 +274,7 @@ function prevEp() {
         var epTitle = showTitles[epNum-1]
         console.log(epID)
         document.querySelector(".video_screen").src = "https://drive.google.com/file/d/" + epID + "/preview"
-        document.querySelector(".episode_num").textContent = localStorage.getItem("chosenShow")
-        document.querySelector(".episode_title").textContent = localStorage.getItem("chosenShow")
+        document.querySelector(".episode_num").textContent = sessionStorage.getItem("chosenShow")
+        document.querySelector(".episode_title").textContent = sessionStorage.getItem("chosenShow")
     }
 }
